6464dc2679308eb9778372ef848fd0dc7a3945e8
govind0832 <53042682+govind0832@users.noreply.github.com>
2025-05-15 19:08:42 +0530

    save get category imlement

---
+++ src/main/java/com/becoder/service/impl/CategoryServiceImpl.java
@@ -0,0 +1,135 @@
+package com.becoder.service.impl;
+
+import java.util.Arrays;
+import java.util.Date;
+import java.util.List;
+import java.util.Optional;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.stereotype.Service;
+import org.springframework.util.ObjectUtils;
+
+import com.becoder.dto.CategoryDto;
+import com.becoder.dto.CategoryResponse;
+import com.becoder.entity.Category;
+import com.becoder.exception.ExistDataException;
+import com.becoder.exception.ResourceNotFoundException;
+import com.becoder.repository.CategoryRepository;
+import com.becoder.service.CacheManagerService;
+import com.becoder.service.CategoryService;
+import com.becoder.util.Validation;
+
+@Service
+public class CategoryServiceImpl implements CategoryService {
+
+	@Autowired
+	private CategoryRepository categoryRepo;
+
+	@Autowired
+	private ModelMapper mapper;
+
+	@Autowired
+	private Validation validation;
+	
+	@Autowired
+	private CacheManagerService cacheService;
+
+	@Override
+	public Boolean saveCategory(CategoryDto categoryDto) {
+
+		// Validation Checking
+		validation.categoryValidation(categoryDto);
+
+		// check category exists or not
+		Boolean exist = categoryRepo.existsByName(categoryDto.getName().trim());
+		if (exist) {
+			// throw error
+			throw new ExistDataException("Category already exist");
+		}
+
+		Category category = mapper.map(categoryDto, Category.class);
+
+		if (ObjectUtils.isEmpty(category.getId())) {
+			category.setIsDeleted(false);
+//			category.setCreatedBy(1);
+			category.setCreatedOn(new Date());
+		} else {
+			updateCategory(category);
+		}
+
+		Category saveCategory = categoryRepo.save(category);
+		if (ObjectUtils.isEmpty(saveCategory)) {
+			return false;
+		}
+		return true;
+	}
+
+	private void updateCategory(Category category) {
+		Optional<Category> findById = categoryRepo.findById(category.getId());
+		if (findById.isPresent()) {
+			Category existCategory = findById.get();
+			category.setCreatedBy(existCategory.getCreatedBy());
+			category.setCreatedOn(existCategory.getCreatedOn());
+			category.setIsDeleted(existCategory.getIsDeleted());
+
+//			category.setUpdatedBy(1);
+//			category.setUpdatedOn(new Date());
+		}
+	}
+
+	@Override
+	@Cacheable("allCategory")
+	public List<CategoryDto> getAllCategory() {
+		List<Category> categories = categoryRepo.findByIsDeletedFalse();
+
+		List<CategoryDto> categoryDtoList = categories.stream().map(cat -> mapper.map(cat, CategoryDto.class)).toList();
+
+		return categoryDtoList;
+	}
+
+	@Override
+	@Cacheable("activeCategory")
+	public List<CategoryResponse> getActiveCategory() {
+
+		List<Category> categories = categoryRepo.findByIsActiveTrueAndIsDeletedFalse();
+		List<CategoryResponse> categoryList = categories.stream().map(cat -> mapper.map(cat, CategoryResponse.class))
+				.toList();
+		return categoryList;
+	}
+
+	@Override
+	@Cacheable(value = "getCategoryById" , key = "#id")
+	public CategoryDto getCategoryById(Integer id) throws Exception {
+
+		Category category = categoryRepo.findByIdAndIsDeletedFalse(id)
+				.orElseThrow(() -> new ResourceNotFoundException("Category not found with id=" + id));
+
+		if (!ObjectUtils.isEmpty(category)) {
+			category.getName().toUpperCase();
+			return mapper.map(category, CategoryDto.class);
+		}
+		return null;
+	}
+
+	@Override
+	@CacheEvict(value = "getCategoryById" , key = "#id")
+	public Boolean deleteCategory(Integer id) {
+		Optional<Category> findByCatgeory = categoryRepo.findById(id);
+
+		if (findByCatgeory.isPresent()) {
+			Category category = findByCatgeory.get();
+			category.setIsDeleted(true);
+			categoryRepo.save(category);
+			
+			// remove from cache
+			cacheService.removeCacheByName(Arrays.asList("allCategory","activeCategory"));
+			
+			return true;
+		}
+		return false;
+	}
+
+}
